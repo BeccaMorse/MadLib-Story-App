3755d831e72c10161ed2d8e8b5690a43109f12cf
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-10-13 18:26:45 -0500

    started maven project; embedded tomcat server and servlet work

---
+++ src/main/java/servlet/MasterServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+		urlPatterns = {
+				""
+		}
+	)
+
+public class MasterServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		ServletOutputStream out = res.getOutputStream();
+		String result = "servlet working";
+		out.write(result.getBytes());
+		out.flush();
+		out.close();
+	}
+
+}
